7030008fd3bc3f7ea5a901ccb50afce77b7e08fc
jkdev <jkdev@DESKTOP-JH35NQ2.localdomain>
2019-10-17 19:04:45 +0200

    hi first commit

---
+++ src/main/java/com/jkdev/placeRest/PlaceRestApplication.java
@@ -0,0 +1,13 @@
+package com.jkdev.placeRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jkdev/placeRest/PlaceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkdev.placeRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
